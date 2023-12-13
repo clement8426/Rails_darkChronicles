017a468202bebabebc201d42a4735d7aadb2b099
clement bastien bastienclement826@gmail.com <bastienclement826@gmail.com>
2023-12-13 10:44:10 +0100

    add link to npc navbar

--- app/controllers/npc_controller.rb
+++ app/controllers/npc_controller.rb
@@ -1,6 +1,7 @@
 class NpcController < ApplicationController
   def index
-    search_params_present = params[:query].values.any?(&:present?)
+    # search_params_present = params[:query].values.any?(&:present?)
+    search_params_present = params[:query].present? && params[:query].values.any?(&:present?)
 
     if search_params_present
       search_criteria = "#{params[:query][:query]} #{params[:query][:clan_category]} #{params[:query][:generation_category]} #{params[:query][:secte_category]}".strip
