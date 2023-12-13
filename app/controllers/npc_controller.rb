1ac0dfd25e24307770e63766b9f3bd8083053128
clement bastien bastienclement826@gmail.com <bastienclement826@gmail.com>
2023-12-13 13:22:28 +0100

    add create part

--- app/controllers/npc_controller.rb
+++ app/controllers/npc_controller.rb
@@ -11,4 +11,26 @@ class NpcController < ApplicationController
       @npcs = Npc.where(user_id: current_user.id)
     end
   end
+
+  def create
+    @npc = current_user.npcs.new(npc_params)
+
+    if @npc.save
+      respond_to do |format|
+        format.html { redirect_to @npc, notice: 'NPC créé avec succès.' }
+        format.json { render json: @npc, status: :created, location: @npc }
+      end
+    else
+      respond_to do |format|
+        format.html { render :new }
+        format.json { render json: @npc.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def npc_params
+    params.require(:new_character).permit(:name, :clan, :secte, :generation, :address, :description)
+  end
 end
